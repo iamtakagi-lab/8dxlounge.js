0e31eef693c037d6b0d4048a58d3e76d1c0da6b7
takagi <iam.takagi.dev@gmail.com>
2021-03-19 00:52:12 +0900

    ✨ src/api.ts: getPlayersByRangeを追記 & コメントを追記

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,12 +7,29 @@ export class LoungeApi {
   private get client() {
     return axios.create({
       baseURL: this.baseUrl,
-      timeout: 5000,
+      timeout: 5000      
     })
   }
 
-  async getPlayers(minMmr: number | null, maxMmr: number | null) {
-    const { data } = await this.client.get<Player[]>(`players/list`, {
+  /**
+   * Get the Players list
+   * @param minMmr 
+   * @param maxMmr 
+   * @returns Players list
+   */
+   async getPlayers() {
+    const { data } = await this.client.get<Player[]>(`player/list`)
+    return data
+  }
+
+  /**
+   * Get the Players list by MMR Range
+   * @param minMmr 
+   * @param maxMmr 
+   * @returns Players list
+   */
+  async getPlayersByRange(minMmr: number, maxMmr: number) {
+    const { data } = await this.client.get<Player[]>(`player/list`, {
       params: {
         minMmr: minMmr,
         maxMmr: maxMmr,
@@ -21,6 +38,11 @@ export class LoungeApi {
     return data
   }
 
+  /**
+   * Get the Player by MKCID
+   * @param mkcId
+   * @returns Player
+   */
   async getPlayerById(mkcId: number) {
     const { data } = await this.client.get<Player>(`player`, {
       params: {
@@ -29,7 +51,12 @@ export class LoungeApi {
     })
     return data
   }
-
+  
+  /**
+   * Get the Player by PlayerName
+   * @param name 
+   * @returns Player
+   */
   async getPlayerByName(name: string) {
     const { data } = await this.client.get<Player>(`player`, {
       params: {
@@ -39,7 +66,12 @@ export class LoungeApi {
     return data
   }
 
-  async getPlayerDetails(name: string) {
+  /**
+   * Get the PlayerDetails by PlayerName
+   * @param name 
+   * @returns PlayerDetails
+   */
+  async getPlayerDetailsByName(name: string) {
     const { data } = await this.client.get<PlayerDetails>(`player/details`, {
       params: {
         name: name,
@@ -48,7 +80,12 @@ export class LoungeApi {
     return data
   }
 
-  async getTable(tableId: number) {
+  /**
+   * Get the Table by TableID
+   * @param tableId 
+   * @returns Table
+   */
+  async getTableById(tableId: number) {
     const { data } = await this.client.get<Table>(`table`, {
       params: {
         tableId: tableId,
@@ -57,12 +94,21 @@ export class LoungeApi {
     return data
   }
 
+  /**
+   * Get the Tables list
+   * @returns Tables list
+   */
   async getTables() {
-    const { data } = await this.client.get<Table>(`table/list`)
+    const { data } = await this.client.get<Table[]>(`table/list`)
     return data
   }
 
-  async getPenalties(name: string) {
+  /**
+   * Get the Penalties list
+   * @param name 
+   * @returns Penalties list
+   */
+  async getPenaltiesByName(name: string) {
     const { data } = await this.client.get<Penalty[]>(`penalty/list`, {
       params: {
         name: name,
